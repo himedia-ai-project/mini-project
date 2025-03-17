25e0d56634b5e20d072ed15bc3bd8aa926a7dc1d
김채림 <rim1080@icloud.com>
2025-03-17 15:25:20 +0900

    fix:resultForm

--- front/src/app/Component/ResultForm.tsx
+++ front/src/app/Component/ResultForm.tsx
@@ -1,8 +1,25 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import Button from "../common/ButtonCommon";
+import axiosInstance from "../lib/axios";
 
 export default function ResultForm() {
+  const [result, setResult] = useState(null);
+
+  useEffect(() => {
+    const fetchResult = async () => {
+      try {
+        const { data } = await axiosInstance.get("/api/result");
+        setResult(data);
+      } catch (error) {
+        console.error("결과 데이터를 불러오는데 실패했습니다.", error);
+      }
+    };
+
+    fetchResult();
+  }, []);
+
   return (
     <div className="w-[500px]">
       <div className="mb-4 p-4 border rounded-md bg-gray-50 shadow-sm">
@@ -18,7 +35,13 @@ export default function ResultForm() {
         </p>
       </div>
       <div className="p-4 border rounded-md bg-gray-50 shadow-sm h-[400px] flex ">
-        <p className="text-black">
+        {/*결과값 불러오기*/}
+        {result ? (
+          <pre>{JSON.stringify(result, null, 2)}</pre>
+        ) : (
+          <p>로딩 중...</p>
+        )}
+        {/* <p className="text-black">
           결과 텍스트 보여주기
           <br />
           <br />
@@ -27,7 +50,7 @@ export default function ResultForm() {
           2.(나이와 목적 특이사항)기반으로 업로드한 파일에서 맞는 top3 성분
           <br />
           3.전체적인 결과
-        </p>
+        </p> */}
       </div>
       <div className="flex justify-center items-center mt-4">
         <Button text="결과 닫기" />
