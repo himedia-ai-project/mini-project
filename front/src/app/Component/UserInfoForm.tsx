4e48eb957ea89203dbebb848b445172723d16266
김채림 <rim1080@icloud.com>
2025-03-17 17:13:37 +0900

    del: 필요없는 파일 삭제

--- front/src/app/Component/UserInfoForm.tsx
+++ front/src/app/Component/UserInfoForm.tsx
@@ -27,35 +27,30 @@ export default function UserInfoForm({
         className="flex flex-col gap-2 items-center w-full max-w-md"
       >
         <InputCommon
-          // label="이름"
           labelName="이름"
           placeholder="이름을 입력해주세요.(ex 홍길동 or 길동)"
           value={formData.name}
           onChange={(e) => setFormData({ ...formData, name: e.target.value })}
         />
         <InputCommon
-          // label="나이"
           labelName="나이"
           placeholder="나이를 입력해주세요.(ex 25살)"
           value={formData.age}
           onChange={(e) => setFormData({ ...formData, age: e.target.value })}
         />
         <InputCommon
-          // label="성별"
           labelName="성별"
           placeholder="성별을 입력해주세요.(ex 여 or 남)"
           value={formData.gender}
           onChange={(e) => setFormData({ ...formData, gender: e.target.value })}
         />
         <InputCommon
-          // label="목적"
           labelName="목적"
           placeholder="목적을 입력해주세요.(ex 감량 운동중)"
           value={formData.object}
           onChange={(e) => setFormData({ ...formData, object: e.target.value })}
         />
         <InputCommon
-          // label="특이사항"
           labelName="특이사항"
           placeholder="특이사항을 입력해주세요.(ex 새우알러지 있음)"
           value={formData.issue}
